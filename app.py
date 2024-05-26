4ee681f10544a3089a5c72074bd63885bd473068
ramsandip <ramsandeep90900@gmail.com>
2024-05-26 15:47:58 +0530

    project completed

--- app.py
+++ app.py
@@ -21,9 +21,20 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    output = model.predict(new_data)[0]
+    #print(output[0])
+    formatted_output = "{:.2f} LAKH".format(output)
+    return jsonify(formatted_output)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    formatted_output = "{:.2f} LAKH".format(output)
+    return render_template("home.html", prediction_text="The house price prediction is: {}".format(formatted_output))
+
 
 if __name__=='__main__':
     app.run(debug=True)
